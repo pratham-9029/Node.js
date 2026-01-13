a8548bb471736c96c6cd4c72b1482a386817022f
pratham-9029 <saixerox5075@gmail.com>
2026-01-13 19:44:46 +0530

    Done

--- Day-07-CRUD/index.js
+++ Day-07-CRUD/index.js
@@ -7,25 +7,69 @@ let users = [];
 
 
 app.use(express.urlencoded({extended:true}))
+app.use(express.json());
 app.set('view engine','ejs')
  
+
+//View HTML
 app.get('/' ,(req,res)=>{
     return res.render('index',{users});
 })
 
+
+//Delete User
+app.get('/delete/usr',(req,res)=>{
+
+    users = users.filter((user)=>{
+        return user.id != parseInt(req.query.id);
+    })
+
+    return res.redirect(req.get('Referrer') ||'/');
+    
+})
+
+
+//Edit User
+app.get('/edit/usr',(req,res)=>{
+    const userId = parseInt(req.query.id);
+    const user = users.find(u => u.id === userId);
+    
+    if(user) {
+        return res.render('edit', { user });
+    } else {
+        return res.redirect('/');
+    }
+})
+
+
+//Add User
 app.post('/create',(req,res)=>{
     
     let user = {
         id : Date.now(),
-        pratham : req.body.text
+        text : req.body.text
     }
     users.push(user);
-
-    console.log(users);
     
-    return res.redirect(req.get('Referrer') ||'/')
+    return res.redirect(req.get('Referrer') ||'/');
 })
 
+//Update User
+app.post('/update',(req,res)=>{
+    const userId = parseInt(req.query.id);
+    const newText = req.body.text;
+    
+    const userIndex = users.findIndex(u => u.id === userId);
+    
+    if(userIndex !== -1) {
+        users[userIndex].text = newText;
+    }
+    
+    return res.redirect('/');
+})
+
+
+//Server Start
 app.listen(PORT,(err)=>{
     if(err){
         console.log(err);
