28ffb390027fa142abe765c289be32eb002a1583
pratham-9029 <saixerox5075@gmail.com>
2026-03-16 12:57:11 +0530

    Day-15

--- Day-15-Pr-PMS/controller/user.controller.js
+++ Day-15-Pr-PMS/controller/user.controller.js
@@ -5,8 +5,39 @@ export const createUser = async(req,res)=>{
     try {
         let salt = await bcrypt.genSalt(10);
         req.body.password = await bcrypt.hash(req.body.password,salt);
-        let user = await userModel.create(req.body);
+        const user = await userModel.create(req.body);
         return res.json(user);
+    } catch (error) {
+        console.log(error.message);
+        return res.json({error:error.message})
+    }
+}
+
+export const getAllUser = async(req,res)=>{
+    try {
+        let users = await userModel.find({});
+        return res.json(users);
+    } catch (error) {
+        console.log(error.message);
+        return res.json({error:error.message})
+    }
+}
+
+export const getUser = async(req,res) =>{
+    try {
+        const {id} = req.params;
+        const user = await userModel.findById(id);
+    } catch (error) {
+        console.log(error.message);
+        return res.json({error:error.message})
+    }
+}
+
+export const deleteUser = async(req,res)=>{
+    try {
+        const {id} = req.params;
+        const dltUser = await userModel.findByIdAndDelete(id);
+        return res.json(dltUser); 
     } catch (error) {
         console.log(error.message);
         return res.json({error:error.message})
--- Day-15-Pr-PMS/routes/api/user.route.js
+++ Day-15-Pr-PMS/routes/api/user.route.js
@@ -1,9 +1,18 @@
 import {Router} from "express";
-import { createUser } from "../../controller/user.controller.js";
+import { createUser, getAllUser, getUser } from "../../controller/user.controller.js";
 
 const userRouter = Router();
 
 //user created
 userRouter.post('/',createUser)
+
+//get all user
+userRouter.get('/', getAllUser);
+
+//get seprate user
+userRouter.get('/:id', getUser)
+
+//delet user
+userRouter.delete('/:id',deleteUser)
 
 export default userRouter;
